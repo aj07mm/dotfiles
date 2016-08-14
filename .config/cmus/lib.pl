5a15af956e8215900421f8a36de4f935d09bbb63
aj07mm <aj07mm@hotmail.com>
2016-08-13 22:06:27 -0300

    cmus stuff on .config

---
+++ .config/cmus/lib.pl
@@ -0,0 +1,80 @@
+/home/aj07mm/Downloads/Krishna Das-Discography [320kbps]/All One/Krishna Das -02-  Refuge in the Name.mp3
+/home/aj07mm/Downloads/Krishna Das-Discography [320kbps]/All One/Krishna Das -03-  Rock in a Heart Space.mp3
+/home/aj07mm/Downloads/Krishna Das-Discography [320kbps]/All One/Krishna Das -04-  Township Krishna.mp3
+/home/aj07mm/Downloads/Krishna Das-Discography [320kbps]/Pilgrim Heart/01-Namah Shivaya.mp3
+/home/aj07mm/Downloads/Krishna Das-Discography [320kbps]/Pilgrim Heart/02-Govinda Hare.mp3
+/home/aj07mm/Downloads/Krishna Das-Discography [320kbps]/Pilgrim Heart/03-Mountain Hare Krishna.mp3
+/home/aj07mm/Downloads/Krishna Das-Discography [320kbps]/Pilgrim Heart/04-Mahamantra Meltdown.mp3
+/home/aj07mm/Downloads/Krishna Das-Discography [320kbps]/Pilgrim Heart/05-Hara Hara Mahadev.mp3
+/home/aj07mm/Downloads/Krishna Das-Discography [320kbps]/Pilgrim Heart/06-Kalabinashini Kali.mp3
+/home/aj07mm/Downloads/Krishna Das-Discography [320kbps]/Pilgrim Heart/07-The Goddest Snite - Mother Song.mp3
+/home/aj07mm/Downloads/Krishna Das-Discography [320kbps]/Pilgrim Heart/08-Devi Puja.mp3
+/home/aj07mm/Downloads/Krishna Das-Discography [320kbps]/Pilgrim Heart/09-Jaya Jagatambe.mp3
+/home/aj07mm/Downloads/Krishna Das-Discography [320kbps]/Pilgrim Heart/10-Yah Devi (This Goddest).mp3
+/home/aj07mm/Downloads/Krishna Das-Discography [320kbps]/Pilgrim Heart/11-Devi _Rave_.mp3
+/home/aj07mm/Downloads/Krishna Das-Discography [320kbps]/Pilgrim Heart/12-The Ring Song.mp3
+/home/aj07mm/Downloads/Krishna Das-Discography [320kbps]/The Greatest Hits Of The Kali Yuga/krishna das_the greatest hits of the kali yuga_01_Bhajelo-ji Hanuman.mp3
+/home/aj07mm/Downloads/Krishna Das-Discography [320kbps]/The Greatest Hits Of The Kali Yuga/krishna das_the greatest hits of the kali yuga_10_Brindavan Hare Ram.mp3
+/home/aj07mm/Downloads/Krishna Das-Discography [320kbps]/Live on Earth/Krishna Das - Live on Earth CD 2/01 - Devi Puja.mp3
+/home/aj07mm/Downloads/Krishna Das-Discography [320kbps]/Live on Earth/Krishna Das - Live on Earth CD 2/02 - Jaya Jagatambe.mp3
+/home/aj07mm/Downloads/Krishna Das-Discography [320kbps]/Live on Earth/Krishna Das - Live on Earth CD 2/03 - Mountain Hare Krishna.mp3
+/home/aj07mm/Downloads/Krishna Das-Discography [320kbps]/Live on Earth/Krishna Das - Live on Earth CD 2/04 - Namah Shivayah.mp3
+/home/aj07mm/Downloads/Krishna Das-Discography [320kbps]/Live on Earth/Krishna Das - Live on Earth CD 2/05 - Rama Bolo.mp3
+/home/aj07mm/Downloads/Krishna Das-Discography [320kbps]/Live on Earth/Krishna Das - Live on Earth CD 2/06 - Shri Krishna Govinda - Gopala.mp3
+/home/aj07mm/Downloads/Krishna Das-Discography [320kbps]/Breath Of The heart/Krishna Das 01 Baba hanuman.mp3
+/home/aj07mm/Downloads/Krishna Das-Discography [320kbps]/Breath Of The heart/Krishna Das 02 Kainchi hare krishna.mp3
+/home/aj07mm/Downloads/Krishna Das-Discography [320kbps]/Breath Of The heart/Krishna Das 03 Ma durga.mp3
+/home/aj07mm/Downloads/Krishna Das-Discography [320kbps]/Breath Of The heart/Krishna Das 04 Kashi vishwanath gange.mp3
+/home/aj07mm/Downloads/Krishna Das-Discography [320kbps]/Breath Of The heart/Krishna Das 05 Om namo bhagavate vasudevaya.mp3
+/home/aj07mm/Downloads/Krishna Das-Discography [320kbps]/Breath Of The heart/Krishna Das 06 Brindavan hare ram.mp3
+/home/aj07mm/Downloads/Krishna Das-Discography [320kbps]/One Track Heart/krishna das - one track heart - 01 - hara hara mahaadeva.mp3
+/home/aj07mm/Downloads/Krishna Das-Discography [320kbps]/One Track Heart/krishna das - one track heart - 02 - devi puja.mp3
+/home/aj07mm/Downloads/Krishna Das-Discography [320kbps]/One Track Heart/krishna das - one track heart - 03 - kali durge.mp3
+/home/aj07mm/Downloads/Krishna Das-Discography [320kbps]/One Track Heart/krishna das - one track heart - 04 - the krishna waltz.mp3
+/home/aj07mm/Downloads/Krishna Das-Discography [320kbps]/One Track Heart/krishna das - one track heart - 05 - hanuman chaleesa.mp3
+/home/aj07mm/Downloads/Krishna Das-Discography [320kbps]/One Track Heart/krishna das - one track heart - 06 - forgiveness (instrumental).mp3
+/home/aj07mm/Downloads/Krishna Das-Discography [320kbps]/One Track Heart/krishna das - one track heart - 07 - prayer to the goddess for forgiveness.mp3
+/home/aj07mm/Downloads/Krishna Das-Discography [320kbps]/One Track Heart/krishna das - one track heart - 08 - hara (mc)krishna.mp3
+/home/aj07mm/Downloads/Krishna Das-Discography [320kbps]/One Track Heart/krishna das - one track heart - 09 - prayer to hanuman.mp3
+/home/aj07mm/Downloads/Krishna Das-Discography [320kbps]/One Track Heart/krishna das - one track heart - 10 - shri ram jai ram.mp3
+/home/aj07mm/Downloads/Krishna Das-Discography [320kbps]/One Track Heart/krishna das - one track heart - 11 - brindavan hare ram.mp3
+/home/aj07mm/Downloads/Krishna Das-Discography [320kbps]/Live on Earth/Krishna Das - Live on Earth CD 1/Radhe Shyam.m4a
+/home/aj07mm/Downloads/Krishna Das-Discography [320kbps]/Live on Earth/Krishna Das - Live on Earth CD 1/Samadhi Sita Ram.m4a
+/home/aj07mm/Downloads/Krishna Das-Discography [320kbps]/Live on Earth/Krishna Das - Live on Earth CD 1/Shri Guru Charanam.m4a
+/home/aj07mm/Downloads/Krishna Das-Discography [320kbps]/Live on Earth/Krishna Das - Live on Earth CD 1/Three Rivers Hare Krishna.m4a
+/home/aj07mm/Downloads/Krishna Das-Discography [320kbps]/Live on Earth/Krishna Das - Live on Earth CD 1/Hanuman Puja.m4a
+/home/aj07mm/Downloads/Krishna Das-Discography [320kbps]/Live on Earth/Krishna Das - Live on Earth CD 1/Hanuman Chaleesa.m4a
+/home/aj07mm/Downloads/Krishna Das-Discography [320kbps]/Live on Earth/Krishna Das - Live on Earth CD 1/Sita Ram.m4a
+/home/aj07mm/Downloads/Krishna Das-Discography [320kbps]/Live on Earth/Krishna Das - Live on Earth CD 1/Jaya Bhagavan.m4a
+/home/aj07mm/Downloads/Krishna Das-Discography [320kbps]/Door Of Faith/01 Puja.mp3
+/home/aj07mm/Downloads/Krishna Das-Discography [320kbps]/Door Of Faith/02 Hey Mata Durga.mp3
+/home/aj07mm/Downloads/Krishna Das-Discography [320kbps]/Door Of Faith/03 Mere Gurudev.mp3
+/home/aj07mm/Downloads/Krishna Das-Discography [320kbps]/Door Of Faith/04 Rudrashtakam (Shiva Stuti).mp3
+/home/aj07mm/Downloads/Krishna Das-Discography [320kbps]/Door Of Faith/05 Jai Jagadisha Hare.mp3
+/home/aj07mm/Downloads/Krishna Das-Discography [320kbps]/Door Of Faith/06 Sri Hanuman Chaleesa.mp3
+/home/aj07mm/Downloads/Krishna Das-Discography [320kbps]/Door Of Faith/07 Hare Ram.mp3
+/home/aj07mm/Downloads/Krishna Das-Discography [320kbps]/The Greatest Hits Of The Kali Yuga/krishna das_the greatest hits of the kali yuga_02_namah shivayah.mp3
+/home/aj07mm/Downloads/Krishna Das-Discography [320kbps]/The Greatest Hits Of The Kali Yuga/krishna das_the greatest hits of the kali yuga_03_ma durga.mp3
+/home/aj07mm/Downloads/Krishna Das-Discography [320kbps]/The Greatest Hits Of The Kali Yuga/krishna das_the greatest hits of the kali yuga_04_hara hara mahadev.mp3
+/home/aj07mm/Downloads/Krishna Das-Discography [320kbps]/The Greatest Hits Of The Kali Yuga/krishna das_the greatest hits of the kali yuga_05_mountain hare krishna.mp3
+/home/aj07mm/Downloads/Krishna Das-Discography [320kbps]/The Greatest Hits Of The Kali Yuga/krishna das_the greatest hits of the kali yuga_06_hanuman baba (dub farm re-mix).mp3
+/home/aj07mm/Downloads/Krishna Das-Discography [320kbps]/The Greatest Hits Of The Kali Yuga/krishna das_the greatest hits of the kali yuga_07_shri guru charanam.mp3
+/home/aj07mm/Downloads/Krishna Das-Discography [320kbps]/The Greatest Hits Of The Kali Yuga/krishna das_the greatest hits of the kali yuga_08_devi puja.mp3
+/home/aj07mm/Downloads/Krishna Das-Discography [320kbps]/The Greatest Hits Of The Kali Yuga/krishna das_the greatest hits of the kali yuga_09_mere guru dev.mp3
+/home/aj07mm/Downloads/Krishna Das-Discography [320kbps]/All One/Krishna Das -01- Calling From Afar.mp3
+/home/aj07mm/Downloads/Krishna Das-Discography [320kbps]/Heart Full Of Soul/Hanuman Prayer.mp3
+/home/aj07mm/Downloads/Krishna Das-Discography [320kbps]/Heart Full Of Soul/Shri Ram Jai Ram Jai Jai Ram.mp3
+/home/aj07mm/Downloads/Krishna Das-Discography [320kbps]/Heart Full Of Soul/Om Namoh Bhagavate Vasudevaya.mp3
+/home/aj07mm/Downloads/Krishna Das-Discography [320kbps]/Heart Full Of Soul/Govinda Hare Gopala Hare.mp3
+/home/aj07mm/Downloads/Krishna Das-Discography [320kbps]/Heart Full Of Soul/Radhe Radhe Shyam.mp3
+/home/aj07mm/Downloads/Krishna Das-Discography [320kbps]/Heart Full Of Soul/Jaya Jagatambe Ma Durga.mp3
+/home/aj07mm/Downloads/Krishna Das-Discography [320kbps]/Heart Full Of Soul/Om Namah Shivaya.mp3
+/home/aj07mm/Downloads/Krishna Das-Discography [320kbps]/Heart Full Of Soul/Jaya Bhagavan (Live).mp3
+/home/aj07mm/Downloads/Krishna Das-Discography [320kbps]/Heart Full Of Soul/Jesus On The Main Line.mp3
+/home/aj07mm/Downloads/Krishna Das-Discography [320kbps]/Heart Full Of Soul/Devakinandan Gopala.mp3
+/home/aj07mm/Downloads/Krishna Das-Discography [320kbps]/Heart Full Of Soul/Goddess Prayer.mp3
+/home/aj07mm/Downloads/Krishna Das-Discography [320kbps]/Heart Full Of Soul/All One (Hare Krishna).mp3
+/home/aj07mm/Downloads/Krishna Das-Discography [320kbps]/A Drop Of The Ocean/01 Lullabye.mp3
+/home/aj07mm/Downloads/Krishna Das-Discography [320kbps]/A Drop Of The Ocean/02 Goddess.mp3
+/home/aj07mm/Downloads/Krishna Das-Discography [320kbps]/A Drop Of The Ocean/03 Devotion.mp3
+/home/aj07mm/Downloads/Krishna Das-Discography [320kbps]/A Drop Of The Ocean/04 Windows.mp3
---
+++ .config/cmus/playlist.pl
@@ -0,0 +1,2 @@
+/home/aj07mm/Downloads/Krishna Das-Discography [320kbps]/Heart Full Of Soul/All One (Hare Krishna).mp3
+/home/aj07mm/Downloads/Krishna Das-Discography [320kbps]/Live on Earth/Krishna Das - Live on Earth CD 1/Samadhi Sita Ram.m4a
